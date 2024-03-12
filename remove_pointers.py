37f5c16f8ceefd78221a32a942f1fa6f594f33c3
csae8092 <peter.andorfer@oeaw.ac.at>
2024-03-12 08:53:30 +0100

    closes #2

---
+++ remove_pointers.py
@@ -0,0 +1,7 @@
+from acdh_tei_pyutils.tei import TeiReader
+
+listperson = "./data/indices/listperson.xml"
+doc = TeiReader(listperson)
+for bad in doc.any_xpath(".//tei:ptr"):
+    bad.getparent().remove(bad)
+doc.tree_to_file(listperson)
