809882196f124d07b506aa97e0160f6e596dce0a
Fernando Sanz-Lázaro <fsanzl@gmail.com>
2024-04-16 16:33:01 +0200

    variable naming

--- scripts/fix_xml.py
+++ scripts/fix_xml.py
@@ -8,22 +8,23 @@ xml_path = "./data/editions"
 xml_files = [f for f in os.listdir(xml_path) if
              f.startswith("staribacher__") and f.endswith(".xml")]
 
-xml_prev = ""
-for xml_file in xml_files:
-    xml_filepath = f"{xml_path}/{xml_file}"
-    xml_doc = TeiReader(xml_filepath)
-    tei_ns = xml_doc.ns_tei
-    xml_doc_root = xml_doc.tree.getroot()
-    xml_doc_root.attrib[f'{{{xml_doc.ns_xml.get("xml")}}}id'] = os.path.splitext(xml_file)[0]
-    if xml_prev:
-        xml_doc_root.attrib['prev'] = xml_prev
-        xml_prev_filepath = f"{xml_path}/{xml_prev}"
-        xml_prev = TeiReader(xml_prev_filepath)
-        xml_prev_root = xml_prev.tree.getroot()
-        xml_prev_root.attrib['next'] = xml_file
-        with open(xml_prev_filepath, "wb") as f:
-            f.write(ET.tostring(xml_prev_root, pretty_print=True))
-    xml_prev = xml_file
 
-    with open(xml_filepath, "wb") as f:
-        f.write(ET.tostring(xml_doc_root, pretty_print=True))
+prev_file = ""
+for current_file in xml_files:
+    current_filepath = os.path.join(xml_path, current_file)
+    xml_current = TeiReader(current_filepath)
+    tei_ns = xml_current.ns_tei
+    xml_current_root = xml_current.tree.getroot()
+    xml_current_root.attrib[f'{{{xml_current.ns_xml.get("xml")}}}id'] = os.path.splitext(current_file)[0]
+    if prev_file:
+        xml_current_root.attrib['prev'] = prev_file
+        prev_filepath = os.path.join(xml_path, prev_file)
+        xml_prev = TeiReader(prev_filepath)
+        xml_prev_root = xml_prev.tree.getroot()
+        xml_prev_root.attrib['next'] = current_file
+        with open(prev_filepath, "wb") as f:
+            f.write(ET.tostring(xml_prev_root, pretty_print=True))
+    prev_file = current_file
+
+    with open(current_filepath, "wb") as f:
+        f.write(ET.tostring(xml_current_root, pretty_print=True))
